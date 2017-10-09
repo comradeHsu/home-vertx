6069dfdb91824b10b7afc23224359cab60b4160d
knockinHeavenDoor <61239289@qq.com>
2017-10-09 23:36:32 +0800

    增加房产数据查询

--- src/main/java/service/HouseServiceImpl.java
+++ src/main/java/service/HouseServiceImpl.java
@@ -3,13 +3,19 @@ package service;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.FindOptions;
 import io.vertx.rx.java.RxHelper;
 import io.vertx.rxjava.ext.mongo.MongoClient;
+import rx.Observable;
 import rx.Single;
+import rx.schedulers.Schedulers;
 
 public class HouseServiceImpl implements HouseService {
 
     private MongoClient mongoClient;
+
+    private final String dataBase = "hourse";
 
     HouseServiceImpl(io.vertx.ext.mongo.MongoClient mongoClient, Handler<AsyncResult<HouseService>> readyHandle){
         this.mongoClient = new MongoClient(mongoClient);
@@ -18,6 +24,13 @@ public class HouseServiceImpl implements HouseService {
 
     @Override
     public HouseService findAllHouseByType(int pageSize, int pageNumber, String type, Handler<AsyncResult<JsonArray>> resultHandler) {
-        return null;
+        JsonObject document = new JsonObject().put("isDeleted","0").put("type",type);
+        FindOptions findOptions = new FindOptions().setSkip(pageNumber*pageSize)
+                .setLimit(pageSize).setSort(new JsonObject().put("createDate", 1));
+        mongoClient.rxFindWithOptions(dataBase,document,findOptions).flatMapObservable(res -> Observable.from(res))
+                .collect(JsonArray::new,JsonArray::add)
+                .subscribeOn(Schedulers.io())
+                .subscribe(RxHelper.toSubscriber(resultHandler));
+        return this;
     }
 }
